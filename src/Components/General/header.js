f8ad42c328e3b84fb0f7bc15129a8645b9d72598
Sten Vallimäe <69051391+StenVallimae@users.noreply.github.com>
2023-01-23 00:39:32 +0200

    Toweit first setup

---
+++ src/Components/General/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <h1>da fuk</h1>
+    </StyledHeader>
+  );
+};
+
+const StyledHeader = styled.div`
+  h1 {
+    color: blue;
+  }
+`;
+
+export default Header;
